7362b5bab1a73fdcbe5454ddab5b8838ace75cbb
Nathaniel Williams <nathaniel.williams@coios.net>
2018-02-15 15:19:51 +0800

    Set up base plugin

---
+++ wp-et-no-google-fonts.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name:       ET No Google Fonts
+ * Plugin URI:        https://github.com/kokone/wp-et-no-google-fonts
+ * Description:       This plugin replaces the Google hosted fonts that Elegant Themes uses and replaces them with locally hosted fonts instead.
+ * Version:           0.0.1
+ * Author:            Nathaniel Williams
+ * Author URI:        http://coios.net/
+ * License:           MIT
+ */
+
+class WP_ET_No_Google_Fonts {
+    
+    public function __construct() {
+		
+	}
+}
+
+new WP_ET_No_Google_Fonts();
